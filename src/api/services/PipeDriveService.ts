1c2c9519c9ae4b782644db4270e13b1b3ebc0901
Gustavo Lima <gustavolimaeduardo@gmail.com>
2022-09-15 14:01:31 -0300

    apikey correta no axios

--- src/api/services/PipeDriveService.ts
+++ src/api/services/PipeDriveService.ts
@@ -1,26 +1,22 @@
 import axios from "axios"
-//const pipedrive = require('pipedrive');
 
-//const defaultClient: any = pipedrive.ApiClient.instance;
-//defaultClient.authentications.api_key.apiKey = process.env.PIPEDRIVE_API_KEY;
+//https://pipedrive.readme.io/docs
+//https://developers.pipedrive.com/docs/api/v1/Persons#addPerson
 const api_token: any = "bf7efeb872f2848fdf7dca2027b969c509d40b41"//process.env.PIPEDRIVE_API_KEY;
 
 export default {
 
     async addPerson(data: any){
         try {
-            //https://pipedrive.readme.io/docs
-             //https://developers.pipedrive.com/docs/api/v1/Persons#addPerson
 
-            //  const api = new pipedrive.DealsApi();
-            //  const response = await api.addperson(data)
-         
-            let response = await axios.post(`https://gustavo-sandbox.pipedrive.com/v1/persons?api_token=${api_token}`,{
+            let response = await axios.post(`https://gustavo-sandbox.pipedrive.com/v1/persons?api_token=${api_token}`,
                 data,
-                headers: {
-                    api_token
-                   }
-            })
+                {
+                    headers: { 
+                        api_token
+                    }
+                }
+            )
             
             return  {status: 'sucesso', result: response.data, code: 200}
 
